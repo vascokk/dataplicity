57d53c51bb103e341e137aa05dfa198ec07db8c4
Will McGugan <willmcgugan@gmail.com>
2014-03-18 14:30:45 +0000

    readmes and version bump

--- dataplicity/constants.py
+++ dataplicity/constants.py
@@ -1,4 +1,4 @@
-SERVER_URL = "https://api.dev.dataplicity.com"
+SERVER_URL = "https://api.dataplicity.com"
 CONF_PATH = "/etc/dataplicity/dataplicity.conf"
 SETTINGS_PATH = "/var/dataplicity/"
 FIRMWARE_PATH = "/srv/dataplicity/fw/"
