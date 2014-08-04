0391ade47b3611ee5e3e00cb0863a929f67f7593
Stuart Marsh <stuartk75@gmail.com>
2014-08-04 11:50:04 +0100

    Fix script

--- dataplicity/rpi.sh
+++ dataplicity/rpi.sh
@@ -14,34 +14,39 @@ has_easy() {
     return 0
 }
 
+install_commands() {
+    # do stuff
+    echo "Installing Dataplicity client...";
+    mkdir -p /etc/dataplicity/
+    mkdir -p /opt/dataplicity/
+
+    if [ "$2" = "--dev" ]; then
+        mkdir /opt/dataplicity/src/
+        cd /opt/dataplicity/src/
+        git clone git@github.com:wildfoundry/dataplicity.git
+        cd /opt/dataplicity/src/dataplicity/
+        python setup.py install
+    else
+        pip install dataplicity
+    fi
+
+    echo "Registering your device";
+    dataplicity init --server https://api.dev.dataplicity.com --rpi --force --usercode $1
+    dataplicity handoff --usercode $1
+    cd /opt/dataplicity/
+    dataplicity registersamplers
+
+    echo "Running samplers and syncing..."
+    dataplicity run
+}
+
 install() {
     if has_easy; then
         if has_pip; then
-            # do stuff
-            echo "Installing Dataplicity client...";
-            mkdir -p /etc/dataplicity/
-            mkdir -p /opt/dataplicity/
-
-            if [ "$2" = "--dev" ]; then
-                mkdir /opt/dataplicity/src/
-                cd /opt/dataplicity/src/
-                git clone git@github.com:wildfoundry/dataplicity.git
-                cd /opt/dataplicity/src/dataplicity/
-                python setup.py install
-            else
-                pip install dataplicity
-            fi
-
-            echo "Registering your device";
-            dataplicity init --server https://api.dev.dataplicity.com --rpi --force --usercode $1
-            dataplicity handoff --usercode $1
-            cd /opt/dataplicity/
-            dataplicity registersamplers
-
-            echo "Running samplers and syncing..."
-            dataplicity run
+            install_commands $1 $2
         else
             easy_install pip
+            install_commands $1 $2
         fi
     else
         echo >&2 "easy_install is required. Please install Python setup tools and try again. Aborting";
