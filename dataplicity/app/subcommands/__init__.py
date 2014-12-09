715f51153c20a272ea8e3d9a930db9ce32e6f2cc
Stuart Marsh <stuartk75@gmail.com>
2014-12-09 14:13:52 +0000

    Add sample subcommand

--- dataplicity/app/subcommands/__init__.py
+++ dataplicity/app/subcommands/__init__.py
@@ -14,4 +14,5 @@ __all__ = ["motd",
            "updateui",
            "handoff",
            "register_samplers",
-           "alert"]
+           "alert",
+           "sample"]
---
+++ dataplicity/app/subcommands/sample.py
@@ -0,0 +1,44 @@
+import logging
+import datetime
+from time import mktime
+from dataplicity.app.subcommand import SubCommand
+
+log = logging.getLogger('dataplicity')
+
+
+class Sample(SubCommand):
+    """ Send an alert """
+    help = __doc__
+
+    def add_arguments(self, parser):
+        parser.add_argument(dest="sampler", help="Sampler name")
+        parser.add_argument(dest="value", help="Sampler value")
+
+    def run(self):
+        args = self.args
+        client = self.app.make_client(log)
+        remote = client.remote
+
+        sampler = args.sampler
+        value = args.value
+        timestamp = mktime(datetime.datetime.utcnow().timetuple())
+
+        with remote.batch() as batch:
+            batch.call_with_id('auth_result',
+                               'device.check_auth',
+                               device_class=client.device_class,
+                               serial=client.serial,
+                               auth_token=client.auth_token)
+            batch.call_with_id("add_sample_result",
+                               "device.add_samples",
+                               device_class=client.device_class,
+                               serial=client.serial,
+                               sampler_name=sampler,
+                               samples=[[timestamp, value]])
+        if not batch.get_result('auth_result'):
+            print "Unable to authenticate with the Dataplicity server, check username and password"
+            return -1
+        if batch.get_result('add_sample_result'):
+            print "sample sent"
+
+
